1d0ecaa2d0114b03f7c6005106a19888fc9c0942
yashwardhan chaudhuri <yashwardhan20417@iiitd.ac.in>
2022-08-01 19:25:06 +0530

    functions added

--- login_system.py
+++ login_system.py
@@ -23,3 +23,36 @@ class User(BaseModel):
     id: int = None
 
 
+class UserInDB(User):
+    hashed_password: str
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI()
+
+def password_verification_bool(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(database, username: str, password: str):
+    user = get_user(database, username)
+    if not user:
+        return False
+    if not password_verification_bool(password, user.hashed_password):
+        return False
+    return user
+
+
