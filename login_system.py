48a73d5404a14df002449e043090891acdd324df
yashwardhan chaudhuri <yashwardhan20417@iiitd.ac.in>
2022-08-01 19:41:39 +0530

    get_current_user created --partial

--- login_system.py
+++ login_system.py
@@ -66,3 +66,22 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     encode["exp"] = expire
     encoded_jwt = jwt.encode(encode, KEY, algorithm="HS256")
     return encoded_jwt
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(database, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
