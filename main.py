f3dae5bca6b3daf918faafed7867ba7026506686
yashwardhan chaudhuri <yashwardhan20417@iiitd.ac.in>
2022-08-02 16:48:05 +0530

    assignment towards completion. Final check pending database linked

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError,jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
+import mysql.connector
+from mysql.connector import Error
 
 # to get a string like this run:
 # openssl rand -hex 32
@@ -13,16 +15,38 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+# accessing database
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='db',
+                                         user='root',
+                                         password='password')
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+    sql_select_Query = "select * from User"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    records = cursor.fetchall()
+
+except mysql.connector.Error as e:
+    print("Error reading data from MySQL table", e)
+
+finally:
+    if connection.is_connected():
+        connection.close()
+        cursor.close()
+        print("MySQL connection is closed")
+
+
+# conversion to a temporary dictionary for fast access
+
+fake_users_db = {}
+for elements in records:
+    fake_users_db[elements[1]] = {
+        "username": elements[1],
+        "hashed_password": elements[3],
         "disabled": False,
     }
-}
+
 
 
 class Token(BaseModel):
