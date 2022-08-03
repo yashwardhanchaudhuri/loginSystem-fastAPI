4f972e561b9d0e973184f823da96440f5722f2b5
yashwardhan chaudhuri <yashwardhan20417@iiitd.ac.in>
2022-08-03 16:29:56 +0530

    final

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#final code
 from datetime import datetime, timedelta
 from typing import Union
 
@@ -9,8 +10,7 @@ from pydantic import BaseModel
 import mysql.connector
 from mysql.connector import Error
 
-# to get a string like this run:
-# openssl rand -hex 32
+
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
